31b33ede9a72e4349d57dd736da5f02e8d5ed1f3
Climate AmanteDeLaVida <git.climate@amante.co>
2017-07-18 23:07:44 -0700

    documentation: fixed typo
    Signed-off-by: Climate AmanteDeLaVida <git.climate@amante.co>

--- bash.docker.custom.functions.sh
+++ bash.docker.custom.functions.sh
@@ -2,7 +2,7 @@
 # version: 0.1.0
 # updated: 07.18.2017
 # about: Dockr is a set of scripts and helper tools to avoid remembering complex docker commands
-# TODO: converstion process into a propper CLI tool with testing
+# TODO: refactor process into a propper CLI tool with testing
 # TODO: create documentation and --help --version commands
 
 
