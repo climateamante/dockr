55dfe85e4a4d445cd3f4d8a44f4ab0de4aea507e
Climate AmanteDeLaVida <git.climate@amante.co>
2017-07-19 05:43:05 -0700

    documentation: updated version number 0.1.2
    version: 0.1.2
    functions added:
    - docker_close_container
    - docker_run
    
    Signed-off-by: Climate AmanteDeLaVida <git.climate@amante.co>

--- bash.docker.custom.functions.sh
+++ bash.docker.custom.functions.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# version: 0.1.0
+# version: 0.1.2
 # updated: 07.18.2017
 # about: Dockr is a set of scripts and helper tools to avoid remembering complex docker commands
 # TODO: converstion process into a propper CLI tool with testing
